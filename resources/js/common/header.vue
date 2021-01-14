569828a5a5e295040eeeb9127d1febbf300a88ea
tatata <mugicha8932@gmail.com>
2021-01-15 02:32:08 +0900

    Disable: headerの一部コメントアウト

--- resources/js/common/header.vue
+++ resources/js/common/header.vue
@@ -12,9 +12,9 @@
                <li class="header__list" style="display: none" :style="homeNav">
                   <a class="header__link" href="#learning"><i class="fas fa-pencil-alt"></i>学習内容</a>
                </li>
-               <li class="header__list" style="display: none" :style="homeNav">
+               <!-- <li class="header__list" style="display: none" :style="homeNav">
                   <a class="header__link" href="#about"><i class="far fa-smile"></i>私について</a>
-               </li>
+               </li> -->
                <!-- <li class="header__list" style="display: none" :style="detailNav">
                   <a class="header__link" href="#function"><i class="fas fa-chalkboard-teacher"></i>内容説明</a>
                </li>
@@ -36,19 +36,18 @@ export default {
          }
          return false;
       },
-      detailNav() {
-         if (this.locationHref !== this.publicPath) {
-            return 'display:flex;';
-         }
-         return false;
-      },
+      // detailNav() {
+      //    if (this.locationHref !== this.publicPath) {
+      //       return 'display:flex;';
+      //    }
+      //    return false;
+      // },
       publicPath() {
          return location.origin + '/';
       },
       locationHref() {
          const before = location.href;
          const after = before.replace(location.hash, '');
-
          return after;
       }
    }
